6ad7ec02e17224642411295f4c7494a69032a7d1
Maksim Baev <i@mbaev.com>
2017-03-19 20:24:35 +0400

    Exclude title Preview

--- app/helpers/md_simple_editor/rails/md_helper.rb
+++ app/helpers/md_simple_editor/rails/md_helper.rb
@@ -8,22 +8,19 @@ module MdSimpleEditor
         content_tag(:div, :id => 'md-editor') do
           content_tag(:div, :class => 'btn-toolbar', role: 'toolbar') do
             header_tags + text_tools + link_and_image_tools +
-                content_tag(:div, class: 'btn-group pull-righ') do
-                  button_tag(:type => 'button', :class => "#{@md_builder.default_class} btn-info preview_md") do
-                    'Предпросмотр'
-                  end
-                end
-          end +
-              content_tag(:div, :id => 'md-text') do
-                yield
-              end +
-              content_tag(:div, class: 'panel panel-success preview-panel', hidden: true) do
-                content_tag(:div, class: 'panel-heading') do
+              content_tag(:div, class: 'btn-group pull-righ') do
+                button_tag(:type => 'button', :class => "#{@md_builder.default_class} btn-info preview_md") do
                   'Предпросмотр'
-                end +
-                    content_tag(:div, :id => 'md-preview', class: 'panel-body') do
-                    end
+                end
               end
+          end +
+            content_tag(:div, :id => 'md-text') do
+              yield
+            end +
+            content_tag(:div, class: 'panel panel-success preview-panel', hidden: true) do
+              content_tag(:div, :id => 'md-preview', class: 'panel-body') do
+              end
+            end
         end
       end
 
@@ -32,18 +29,18 @@ module MdSimpleEditor
           button_tag(type: 'button', class: "#{@md_builder.default_class} md_h1") do
             content_tag(:strong, 'H1')
           end +
-              button_tag(type: 'button', class: "#{@md_builder.default_class} md_h2") do
-                content_tag(:strong, 'H2')
-              end +
-              button_tag(type: 'button', class: "#{@md_builder.default_class} md_h3") do
-                content_tag(:strong, 'H3')
-              end +
-              button_tag(type: 'button', class: "#{@md_builder.default_class} md_h4") do
-                content_tag(:strong, 'H4')
-              end +
-              button_tag(type: 'button', class: "#{@md_builder.default_class} md_h5") do
-                content_tag(:strong, 'H5')
-              end
+            button_tag(type: 'button', class: "#{@md_builder.default_class} md_h2") do
+              content_tag(:strong, 'H2')
+            end +
+            button_tag(type: 'button', class: "#{@md_builder.default_class} md_h3") do
+              content_tag(:strong, 'H3')
+            end +
+            button_tag(type: 'button', class: "#{@md_builder.default_class} md_h4") do
+              content_tag(:strong, 'H4')
+            end +
+            button_tag(type: 'button', class: "#{@md_builder.default_class} md_h5") do
+              content_tag(:strong, 'H5')
+            end
         end
       end
 
@@ -52,9 +49,9 @@ module MdSimpleEditor
           button_tag(:type => 'button', :class => "#{@md_builder.default_class} md_link") do
             fa_icon 'link'
           end +
-              button_tag(:type => 'button', :class => "#{@md_builder.default_class} md_camera-retro") do
-                fa_icon 'camera-retro'
-              end
+            button_tag(:type => 'button', :class => "#{@md_builder.default_class} md_camera-retro") do
+              fa_icon 'camera-retro'
+            end
         end
       end
 
@@ -63,30 +60,30 @@ module MdSimpleEditor
           button_tag(type: 'button', class: "#{@md_builder.default_class} md_italic") do
             fa_icon 'italic'
           end +
-              button_tag(type: 'button', class: "#{@md_builder.default_class} md_bold") do
-                fa_icon 'bold'
-              end +
-              button_tag(:type => 'button', class: "#{@md_builder.default_class} md_list-ul") do
-                fa_icon 'list-ul'
-              end +
-              button_tag(:type => 'button', class: "#{@md_builder.default_class} md_list-ol") do
-                fa_icon 'list-ol'
-              end +
-              button_tag(:type => 'button', class: "#{@md_builder.default_class} md_indent") do
-                fa_icon 'indent'
-              end +
-              button_tag(:type => 'button', class: "#{@md_builder.default_class} md_underline") do
-                fa_icon 'underline'
-              end +
-              button_tag(:type => 'button', class: "#{@md_builder.default_class} md_table") do
-                fa_icon 'table'
-              end +
-              button_tag(:type => 'button', class: "#{@md_builder.default_class} md_square") do
-                fa_icon 'square'
-              end +
-              button_tag(:type => 'button', class: "#{@md_builder.default_class} md_minus") do
-                fa_icon 'minus'
-              end
+            button_tag(type: 'button', class: "#{@md_builder.default_class} md_bold") do
+              fa_icon 'bold'
+            end +
+            button_tag(:type => 'button', class: "#{@md_builder.default_class} md_list-ul") do
+              fa_icon 'list-ul'
+            end +
+            button_tag(:type => 'button', class: "#{@md_builder.default_class} md_list-ol") do
+              fa_icon 'list-ol'
+            end +
+            button_tag(:type => 'button', class: "#{@md_builder.default_class} md_indent") do
+              fa_icon 'indent'
+            end +
+            button_tag(:type => 'button', class: "#{@md_builder.default_class} md_underline") do
+              fa_icon 'underline'
+            end +
+            button_tag(:type => 'button', class: "#{@md_builder.default_class} md_table") do
+              fa_icon 'table'
+            end +
+            button_tag(:type => 'button', class: "#{@md_builder.default_class} md_square") do
+              fa_icon 'square'
+            end +
+            button_tag(:type => 'button', class: "#{@md_builder.default_class} md_minus") do
+              fa_icon 'minus'
+            end
         end
       end
     end
